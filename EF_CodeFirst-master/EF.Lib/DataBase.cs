31025125c347590848db91bdcb84702ac7e42b5a
Константин <86431195+Cernim53@users.noreply.github.com>
2022-08-27 08:42:19 +0200

    Add files via upload

---
+++ EF_CodeFirst-master/EF.Lib/DataBase.cs
@@ -0,0 +1,31 @@
+﻿
+using EF.Model;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+
+namespace EF.Lib
+{
+    public class DataBase : DbContext
+    {
+        public static string connectionStringFile => "/ConnectionString.txt";
+        public DbSet<Game> TabGames { get; set; }
+
+        protected DataBase() { }
+        public DataBase(DbContextOptions<DataBase> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public static DataBase Init()
+        {
+            var options = new DbContextOptionsBuilder<DataBase>().UseMySQL(GetConnectionString(connectionStringFile)).Options;
+            return new DataBase(options);
+        }
+
+        public static string GetConnectionString(string connectionStringFile)
+        {
+            var streamReader = new StreamReader(Directory.GetCurrentDirectory() + connectionStringFile);
+            return streamReader.ReadToEnd();
+        }
+
+    }
+}
